15650879825168de6f1fafffa5be15b7c6ba737c
Maedeh Sadat Khorasani <maedeh.skh80@gmail.com>
2024-01-26 17:26:27 +0330

    learning lists

--- 6lists.py
+++ 6lists.py
@@ -1,7 +1,20 @@
-numbers = [3,6,2,8,4,10]
-max = numbers[0]
-for number in numbers:
-    if number > max:
-        max = number
+# 1D lists
+# numbers = [3,6,2,8,4,10]
+# max = numbers[0]
+# for number in numbers:
+#     if number > max:
+#         max = number
 
-print (max)
+# print (max)
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+# matrix [0][1]=20
+# print (matrix[0][1])
+for row in matrix:
+    for item in row:
+        print(item)
+
