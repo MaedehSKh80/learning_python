1901a926e90e02617d9e6ec2aa82b83d4a57c6cc
Maedeh Sadat Khorasani <maedeh.skh80@gmail.com>
2024-02-04 19:17:24 +0330

    readbilitie

--- 10definingFunctions.py
+++ 10definingFunctions.py
@@ -1,6 +1,6 @@
-def greeting (name):
-    print (f"hello {name}, welcome\n")
+def greeting (firstName, lastName):
+    print (f"\n hello {firstName} {lastName}, welcome\n")
 
 print ("start\n")
-greeting(input('input your name: '))
+greeting(firstName=input('input your first name: \n') ,lastName=input('input your last name: \n' ))
 print ("finished\n")
