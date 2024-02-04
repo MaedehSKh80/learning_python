d7c93516c30eaf34957692fb66d1d3cb0d019a5a
Maedeh Sadat Khorasani <maedeh.skh80@gmail.com>
2024-02-04 19:08:38 +0330

    name input for greeting

--- 10definingFunctions.py
+++ 10definingFunctions.py
@@ -1,6 +1,6 @@
-def greeting ():
-    print ("hello dear, welcome\n")
+def greeting (name):
+    print (f"hello {name}, welcome\n")
 
 print ("start\n")
-greeting()
+greeting(input('input your name '))
 print ("finished\n")
