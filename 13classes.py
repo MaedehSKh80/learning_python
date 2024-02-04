50518fc395adc45a2111898e81a14d8d26d1783c
Maedeh Sadat Khorasani <maedeh.skh80@gmail.com>
2024-02-04 19:57:51 +0330

    learning abit about classes

---
+++ 13classes.py
@@ -0,0 +1,26 @@
+# class Point:
+#     def __init__(self, x , y) :
+#         self.x=x
+#         self.y=y
+
+#     def mov(self):
+#         print('move')
+
+#     def draw (self):
+#         print('draw')
+# Point = Point (40,20) 
+# print (Point.x)
+
+class Person:
+    def __init__(self, name) :
+        self.name = name
+
+    def talk(self):
+        print(f"hello, I'm {self.name}")
+
+john = Person("John smith")
+john.talk()
+
+Bob = Person("Bob smith")
+Bob.talk()
+
