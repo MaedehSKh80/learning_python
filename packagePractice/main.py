eeee8872b961efd72a80aeeb68be65dd83126fa2
Maedeh Sadat Khorasani <maedeh.skh80@gmail.com>
2024-02-07 20:52:31 +0330

    practicing packages

---
+++ packagePractice/main.py
@@ -0,0 +1,13 @@
+#how to import packages in diffrent ways
+
+# importing file inside of folder
+#from packagePractice import shipping
+
+# importing module inside of file of a folder
+# from packagePractice.shipping import calc_shipping
+
+# importing full folder
+# import packagePractice
+
+# importing full file
+# import packagePractice.shipping
